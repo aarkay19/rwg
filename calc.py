a2d4953197e00152c996bb5e9b953a5b0a2492d1
aarkay19 <aarkay@gmail.com>
2019-09-14 22:23:09 -0700

    Initial Commit-checks

---
+++ calc.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+class Employee:
+   """Employee class"""
+   def __init__(self, first, last):
+      self.last = last
+      self.first = first
+
+   def email(self):
+      self.email = "{}.{}@gmail.com".format(self.first,self.last)
+      return self.email
+
+emp_1 = Employee('aar', 'kay')
+
+#print(emp_1.last)
+print(emp_1.first)
+print(emp_1.email())
